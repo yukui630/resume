f5693ecbffeaeae86b5005379e53a01707505572
huanglu <348291442@qq.com>
2017-02-22 18:06:31 +0800

    再次修改链接

--- resume/bundle.js
+++ resume/bundle.js
@@ -10911,22 +10911,6 @@ jQuery(document).ready(function ($) {
 		$('.responsive_menu').hide();
 	});
 });
-
-// function loadScript() {
-//   var script = document.createElement('script');
-//   script.type = 'text/javascript';
-//   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-//       'callback=initialize';
-//   document.body.appendChild(script);
-// }
-
-// function initialize() {
-//     var mapOptions = {
-//       zoom: 12,
-//       center: new google.maps.LatLng(40.7823234,-73.9654161)
-//     };
-//     var map = new google.maps.Map(document.getElementById('templatemo_map'),  mapOptions);
-// }
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
